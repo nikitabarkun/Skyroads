8216538cde23c1071e20b5ed5bee2664c0cbd203
nbarkun <nikitaba@playtika.com>
2020-10-26 17:14:21 +0300

    Imported audio importer

---
+++ Skyroads/Assets/Editor/AudioPostprocessor.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AudioPostprocessor : AssetPostprocessor
+{
+    private const int MinSizeForStreaming = 5242880;
+    
+    void OnPreprocessAudio()
+    {
+        var fileInfo = new FileInfo(assetPath);
+
+        if (fileInfo.Length > MinSizeForStreaming)
+        {
+            AudioImporter audioImporter = (AudioImporter)assetImporter;
+        
+            var settings = new AudioImporterSampleSettings();
+            settings.loadType = AudioClipLoadType.Streaming;
+            
+            audioImporter.defaultSampleSettings = settings;
+        }
+    }
+}
