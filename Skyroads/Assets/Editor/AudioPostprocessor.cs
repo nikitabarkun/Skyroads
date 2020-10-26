3059e555a3643f5d8918db6ff4de8827d3a9cb39
nbarkun <nikitaba@playtika.com>
2020-10-26 17:22:57 +0300

    Changed logics

--- Skyroads/Assets/Editor/AudioPostprocessor.cs
+++ Skyroads/Assets/Editor/AudioPostprocessor.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class AudioPostprocessor : AssetPostprocessor
 {
+    private const int MinSizeForCompressedInMemory = 204800;
     private const int MinSizeForStreaming = 5242880;
     
     void OnPreprocessAudio()
     {
         var fileInfo = new FileInfo(assetPath);
 
+        AudioImporter audioImporter = (AudioImporter)assetImporter;
+        
+        var settings = new AudioImporterSampleSettings();
+        
         if (fileInfo.Length > MinSizeForStreaming)
         {
-            AudioImporter audioImporter = (AudioImporter)assetImporter;
-        
-            var settings = new AudioImporterSampleSettings();
             settings.loadType = AudioClipLoadType.Streaming;
-            
-            audioImporter.defaultSampleSettings = settings;
         }
+        else if (fileInfo.Length > MinSizeForCompressedInMemory)
+        {
+            settings.loadType = AudioClipLoadType.CompressedInMemory;
+        }
+        else
+        {
+            settings.loadType = AudioClipLoadType.DecompressOnLoad;
+        }
+        
+        audioImporter.defaultSampleSettings = settings;
     }
 }
